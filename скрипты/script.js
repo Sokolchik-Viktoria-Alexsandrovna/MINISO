603dc6fe66388f704dce2c969ef20bb13114bd0c
viktoria <viktoriasokolchik@gmail.com>
2024-05-10 02:35:14 +0300

    Added project

---
+++ скрипты/script.js
@@ -0,0 +1,71 @@
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
+
+
+document.getElementById("main-action").onclick = function () {
+    document.getElementById("cars").scrollIntoView({behavior: "smooth"});
+}
+
+var buttons = document.getElementsByClassName("car-button");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("price").scrollIntoView({behavior: "smooth"});
+    }
+}
+
+document.getElementById("price-action").onclick = function () {
+    if (document.getElementById("name").value === "") {
+        alert("Заполните поле имя!");
+    } else if (document.getElementById("phone").value === "") {
+        alert("Заполните поле Телефон!");
+    } else if (document.getElementById("car").value === "") {
+        alert("Заполните поле Автомобиль!");
+    } else {
+        alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!")
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    let layer = document.querySelector('.price-img');
+    document.addEventListener('mousemove', (event) => {
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.3) / 8) + 'px,' + ((event.clientY * 0.3) / 8) + 'px,0px)';
+    });
+
+    const elem = document.querySelector(".main");
+    document.addEventListener('scroll', () => {
+        elem.style.backgroundPositionX = '0' + (0.3 * window.pageYOffset) + 'px';
+    })
+});
